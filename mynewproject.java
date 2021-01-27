1f5154aa83106d780d1fbb18b87932bef4f90394
Marie Ready <readyelwin@gmail.com>
2021-01-26 22:57:09 -0500

    changes

--- mynewproject.java
+++ mynewproject.java
@@ -1,10 +1,16 @@
 package mynewproject.mynewproject.bin;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class mynewproject {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner in = new Scanner (new File("test.txt");
+		
+		while(in.hasNextLine()) {
+			System.out.println(in.nextLine());
+		}
+		
+		in.close();
 }
